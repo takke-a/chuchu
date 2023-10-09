721aa8d7cf48752a3cf5da1e8757bfb77983fa78
takke <107464146+takke-a@users.noreply.github.com>
2023-10-09 15:52:39 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+// JavaScript
+
+//画面が読み込まれたら animation を呼び出す
+//window.addEventListener('load', animation);
+window.setTimeout( animation, 3000 );
+
+// 定数の定義
+const loading = document.getElementById('loading');
+
+// cssクラスを追加する関数
+function animation(){
+  loading.classList.add('loaded');
+}
